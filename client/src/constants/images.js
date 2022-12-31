317c142c2bdcef417ccd2ff128cdaac9567020d7
Cayla Bradley <cayla.bradley94@gmail.com>
2022-12-30 21:52:39 -0600

    import image constants

---
+++ client/src/constants/images.js
@@ -0,0 +1,87 @@
+import api from '../assets/images/api.png';
+import basicPortfolio from '../assets/images/basic-portfolio.png';
+import bootstrap from '../assets/images/bootstrap.png';
+import csharp from '../assets/images/csharp.png';
+import css from '../assets/images/css.png';
+import django from '../assets/images/django.png';
+import entityFramework from '../assets/images/entityFramework.png';
+import express from '../assets/images/express.png';
+import figma from '../assets/images/figma.png';
+import flask from '../assets/images/flask.png';
+import flutter from '../assets/images/flutter.png';
+import forHire from '../assets/images/unemployed-woman-with-cv-waiting-for-job-opportunity.png';
+import framerMotion from '../assets/images/framerMotion.png';
+import gameDevelopment from '../assets/images/gameDevelopment.png';
+import git from '../assets/images/git.png';
+import github from '../assets/images/github.png';
+import go from '../assets/images/go.png';
+import graphql from '../assets/images/graphql.png';
+import html from '../assets/images/html.png';
+import javascript from '../assets/images/javascript.png';
+import developer from '../assets/images/man-developing-website-on-desk.svg';
+import mongo from '../assets/images/mongo.png';
+import mongoose from '../assets/images/mongoose.png';
+import mu5 from '../assets/images/mu5.png';
+import MySQL from '../assets/images/MySQL.png';
+import node from '../assets/images/node.png';
+import parkayFloor from '../assets/images/parkay-floor.png';
+import programmer from '../assets/images/programmer.png';
+import programmer1 from '../assets/images/programmer1.png';
+import programmer2 from '../assets/images/programmer2.png';
+import python from '../assets/images/python.png';
+import react from '../assets/images/react.png';
+import redux from '../assets/images/redux.png';
+import sanity from '../assets/images/sanity.png';
+import sass from '../assets/images/sass.png';
+import softwareDevelopment from '../assets/images/softwareDevelopment.png';
+import softwareDevelopment1 from '../assets/images/softwareDevelopment1.png';
+import softwareDevelopment2 from '../assets/images/softwareDevelopment2.png';
+import sqlAlchemy from '../assets/images/sqlAlchemy.png';
+import typescript from '../assets/images/typescript.png';
+import uiux from '../assets/images/uiux.png';
+import vue from '../assets/images/vue.png';
+
+export default {
+    api,
+    basicPortfolio,
+    bootstrap,
+    csharp,
+    css,
+    django,
+    entityFramework,
+    express,
+    figma,
+    flask,
+    flutter,
+    forHire,
+    framerMotion,
+    gameDevelopment,
+    git,
+    github,
+    go,
+    graphql,
+    html,
+    javascript,
+    developer,
+    mongo,
+    mongoose,
+    mu5,
+    MySQL,
+    node,
+    parkayFloor,
+    programmer,
+    programmer1,
+    programmer2,
+    python,
+    react,
+    redux,
+    sanity,
+    sass,
+    softwareDevelopment,
+    softwareDevelopment1,
+    softwareDevelopment2,
+    sqlAlchemy,
+    typescript,
+    uiux,
+    vue
+}
