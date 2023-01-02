18c678e1e3d5f7c033d91dd7d1425df865431a7c
Cayla Bradley <cayla.bradley94@gmail.com>
2023-01-02 15:42:59 -0600

    style skills component

---
+++ client/src/constants/skillsData.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import { images } from '../constants'
+
+const skillsData = () => {
+    return (
+        [
+            { 
+                name: 'JavaScript',
+                src: images.javascript,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'Python',
+                src: images.python,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'C#',
+                src: images.csharp,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'Go',
+                src: images.go,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'HTML',
+                src: images.html,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'CSS',
+                src: images.css,
+                category: ['All', 'Languages'],
+            },
+            { 
+                name: 'Bootstrap',
+                src: images.bootstrap,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'SASS/SCSS',
+                src: images.sass,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'React.js',
+                src: images.react,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Vue.js',
+                src: images.vue,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Node.js',
+                src: images.node,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Express.js',
+                src: images.express,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Mongoose',
+                src: images.mongoose,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'MongoDB',
+                src: images.mongo,
+                category: ['All', 'Databases'],
+            },
+            { 
+                name: 'Material UI',
+                src: images.materialUI,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Framer-Motion',
+                src: images.framerMotion,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'MySQL',
+                src: images.MySQL,
+                category: ['All', 'Databases'],
+            },
+            { 
+                name: 'Flask',
+                src: images.flask,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Django',
+                src: images.django,
+                category: ['All', 'Frameworks/Libraries'],
+            },
+            { 
+                name: 'Git',
+                src: images.git,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'GitHub',
+                src: images.github,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'Restful Api',
+                src: images.api,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'SQLAlchemy',
+                src: images.sqlAlchemy,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'GraphQL',
+                src: images.graphql,
+                category: ['All', 'Other'],
+            },
+            { 
+                name: 'Sanity',
+                src: images.sanity,
+                category: ['All', 'Other'],
+            },
+        ]
+    )
+}
+
+export default skillsData
--- client/src/container/Skills/Skills.jsx
+++ client/src/container/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 
 import { Wrapper } from '../../hocs'
 import { Button } from '../../components'
-import { images } from '../../constants'
+import { skillsData } from '../../constants'
 import './Skills.scss'
 
 const Skills = () => {
@@ -13,134 +13,6 @@ const Skills = () => {
     const [filterSkills, setFilterSkills] = useState([])
 
     useEffect(() => {
-        const skillsData = [
-            { 
-                name: 'JavaScript',
-                src: images.javascript,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'Python',
-                src: images.python,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'C#',
-                src: images.csharp,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'Go',
-                src: images.go,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'HTML',
-                src: images.html,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'CSS',
-                src: images.css,
-                category: ['All', 'Languages'],
-            },
-            { 
-                name: 'Bootstrap',
-                src: images.bootstrap,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'SASS/SCSS',
-                src: images.sass,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'React.js',
-                src: images.react,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Vue.js',
-                src: images.vue,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Node.js',
-                src: images.node,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Express.js',
-                src: images.express,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Mongoose',
-                src: images.mongoose,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'MongoDB',
-                src: images.mongo,
-                category: ['All', 'Databases'],
-            },
-            { 
-                name: 'Material UI',
-                src: images.materialUI,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Framer-Motion',
-                src: images.framerMotion,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'MySQL',
-                src: images.MySQL,
-                category: ['All', 'Databases'],
-            },
-            { 
-                name: 'Flask',
-                src: images.flask,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'Django',
-                src: images.django,
-                category: ['All', 'Frameworks/Libraries'],
-            },
-            { 
-                name: 'SQLAlchemy',
-                src: images.sqlAlchemy,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'Git',
-                src: images.git,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'GitHub',
-                src: images.github,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'Restful Api',
-                src: images.api,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'GraphQL',
-                src: images.graphql,
-                category: ['All', 'Other'],
-            },
-            { 
-                name: 'Sanity',
-                src: images.sanity,
-                category: ['All', 'Other'],
-            },
-        ]
-
         setSkills(skillsData);
         setFilterSkills(skillsData)
     }, [])
@@ -179,7 +51,7 @@ const Skills = () => {
                                     key={index}
                                     onClick={() => handleSkillsFilter(item)}
                                     text={item}
-                                    classes={`tile-filter__item btn btn--primary ${activeFilter === item ? 'active' : '' }`}
+                                    classes={`tile-filter__item btn btn--filter ${activeFilter === item ? 'active' : '' }`}
                                     type='filter'
                                 />
                             ))
